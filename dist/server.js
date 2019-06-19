f481abd82f1ef3a4b3626b20074bce9eedc870a4
Chhen Rina <chhenrina@gmail.com>
2019-06-20 00:01:33 +0700

    commit deploy

--- dist/server.js
+++ dist/server.js
@@ -2,7 +2,7 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 require("reflect-metadata");
 const app_1 = require("./app");
-const PORT = 3002;
+const PORT = 8081;
 app_1.default.listen(PORT, () => {
     console.log('Server start listening on port ' + PORT);
 });
